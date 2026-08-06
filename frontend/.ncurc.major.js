6dfc08199886ce5a135e0104a5a49656ffa77b1e
YourivHDenHaag <108077148+YourivHDenHaag@users.noreply.github.com>
2026-08-06 10:54:07 +0200

    chore: major updates without graphql + linting fixes + cleanup unused code

--- frontend/.ncurc.major.js
+++ frontend/.ncurc.major.js
@@ -1,7 +1,17 @@
 const minorConfig = require("./.ncurc.minor");
 
+// Keep ESLint on v9, since a lot of the eslint plugins are not yet compatible with v10.
+
 module.exports = {
   ...minorConfig,
-  reject: [...minorConfig.reject],
+  reject: [
+    ...minorConfig.reject,
+    "eslint",
+    "typescript",
+    "graphql",
+    "@graphql-codegen/cli",
+    "@graphql-codegen/client-preset",
+    "@graphql-typed-document-node/core",
+  ],
   target: "latest",
 };
