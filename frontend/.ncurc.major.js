d993473122eb91a01126298a4541a8a6d6040a43
YourivHDenHaag <108077148+YourivHDenHaag@users.noreply.github.com>
2026-08-11 12:46:22 +0200

    chore: upgraded typescript from 5 to 6 + some other minor updates

--- frontend/.ncurc.major.js
+++ frontend/.ncurc.major.js
@@ -1,6 +1,7 @@
 const minorConfig = require("./.ncurc.minor");
 
 // Keep ESLint on v9, since a lot of the eslint plugins are not yet compatible with v10.
+// Typescript v7 is not yet supported by @typescript-eslint plugins, so we keep it on v6 for now.
 
 module.exports = {
   ...minorConfig,
