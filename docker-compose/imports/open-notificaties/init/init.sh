a4abe65222264311db0b306348e4588ebb5e714a
Anna Vissart <anna.vissart@ritense.com>
2026-08-25 09:07:56 +0200

    feat: docker compose cleanup pt1

--- docker-compose/imports/open-notificaties/init/init.sh
+++ docker-compose/imports/open-notificaties/init/init.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-
 echo ">>>>  NL Portal init script: Open Notificaties <<<<"
-sleep 2
-while true
-do
-    if pg_isready -h $DB_HOST -q && python /app/src/manage.py migrate --check >/dev/null 2>&1
-        then
-            echo "Database ready."
-            echo "Loading fixtures:"
-            sleep 2
-            python /app/src/manage.py loaddata configuration kanalen
-            break
-        else
-            echo "Database is not ready. Retrying in 10 seconds."
-            sleep 10
-    fi
+until python /app/src/manage.py migrate --check >/dev/null 2>&1; do
+    echo "Waiting for opennotificaties migrations..."
+    sleep 5
 done
-
+python /app/src/manage.py loaddata configuration kanalen
 echo ">>>> Done <<<<"
